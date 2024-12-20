f24c53f672d5f0656f32a81f84b3236d675fca69
jamilur <jamilur.nutech@gmail.com>
2024-12-20 18:55:53 +0700

    bean

--- src/test/java/rangkotodotcom/spring/core/BeanTest.java
+++ src/test/java/rangkotodotcom/spring/core/BeanTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import rangkotodotcom.spring.core.data.Foo;
 
 public class BeanTest {
     @Test
@@ -13,5 +14,13 @@ public class BeanTest {
         Assertions.assertNotNull(context);
     }
 
+    @Test
+    void testGetBean() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 
+        Foo foo1 = context.getBean(Foo.class);
+        Foo foo2 = context.getBean(Foo.class);
+
+        Assertions.assertSame(foo1, foo2);
+    }
 }
